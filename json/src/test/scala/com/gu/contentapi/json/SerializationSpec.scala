82fb5735a0d0d1dd932a69a9c32118177bcec1b6
Chris Birchall <chris.birchall@guardian.co.uk>
2016-04-26 18:49:06 +0100

    Add serialization round-trip tests for more response types

--- json/src/test/scala/com/gu/contentapi/json/SerializationSpec.scala
+++ json/src/test/scala/com/gu/contentapi/json/SerializationSpec.scala
@@ -1,8 +1,8 @@
 package com.gu.contentapi.json
 
-import com.gu.contentapi.client.model.v1.TagsResponse
+import com.gu.contentapi.client.model.v1._
 import com.gu.contentapi.json.utils.JsonLoader
-import org.json4s.{Diff, Extraction}
+import org.json4s.{JValue, Diff, Extraction}
 import org.json4s.JsonAST.{JArray, JNothing, JObject}
 import org.json4s.jackson.JsonMethods
 import org.scalatest._
@@ -21,14 +21,56 @@ class SerializationSpec extends FlatSpec with Matchers {
     marked as required, making the Thrift model inconsistent with the JSON one.
     We work around this problem in Concierge.
      */
-    val jsonAfterWithReferencesRemoved = jsonAfter.removeField {
-      case ("references", JArray(Nil)) => true
-      case _ => false
+    val jsonAfterWithReferencesRemoved = jsonAfter.transformField {
+      case ("results", JArray(results)) =>
+        val withoutRefs = results.map(_.removeField {
+          case ("references", JArray(Nil)) => true
+          case _ => false
+        })
+        ("results", JArray(withoutRefs))
     }
 
-    val diff = Diff.diff(jsonBefore, jsonAfterWithReferencesRemoved)
+    checkDiff(jsonBefore, jsonAfterWithReferencesRemoved)
+  }
 
-    if (diff != Diff(JNothing, JNothing, JNothing)) {
+  it should "round-trip a SectionsResponse" in {
+    checkRoundTrip[SectionsResponse]("sections.json")
+  }
+
+  it should "round-trip a RemovedContentResponse" in {
+    checkRoundTrip[RemovedContentResponse]("removed.json")
+  }
+
+  it should "round-trip an EditionsResponse" in {
+    checkRoundTrip[EditionsResponse]("editions.json")
+  }
+
+  it should "round-trip an ErrorResponse" in {
+    checkRoundTrip[ErrorResponse]("error.json")
+  }
+
+  it should "round-trip a Thrift Enum with a complex name" in {
+    val tagTypeBefore = TagType.NewspaperBookSection
+    val json = Extraction.decompose(tagTypeBefore)
+    val tagTypeAfter = json.extract[TagType]
+
+    tagTypeAfter should equal(tagTypeBefore)
+  }
+
+  val Identical = Diff(JNothing, JNothing, JNothing)
+
+  def checkRoundTrip[T: Manifest](jsonFileName: String) = {
+    val jsonBefore = JsonMethods.parse(JsonLoader.loadJson(jsonFileName))
+    val deserialized = (jsonBefore \ "response").extract[T]
+    val jsonAfter = JObject("response" -> Extraction.decompose(deserialized))
+
+    checkDiff(jsonBefore, jsonAfter)
+  }
+
+  def checkDiff(jsonBefore: JValue, jsonAfter: JValue) = {
+    val diff = Diff.diff(jsonBefore, jsonAfter)
+
+    if (diff != Identical) {
       println("JSON before:")
       println(JsonMethods.pretty(jsonBefore))
       println("=====")
@@ -45,6 +87,8 @@ class SerializationSpec extends FlatSpec with Matchers {
       println(JsonMethods.pretty(diff.changed))
       println("=====")
     }
+
+    diff should be(Identical)
   }
 
 }
