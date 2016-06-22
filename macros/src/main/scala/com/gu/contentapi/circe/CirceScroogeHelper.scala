dbff1674fd2ac677c927aa4105676d293a2e3749
Chris Birchall <chris.birchall@guardian.co.uk>
2016-06-22 17:13:13 +0100

    Add a macro to provide Circe Decoders for Thrift enums

--- macros/src/main/scala/com/gu/contentapi/circe/CirceScroogeHelper.scala
+++ macros/src/main/scala/com/gu/contentapi/circe/CirceScroogeHelper.scala
@@ -1,46 +1,50 @@
 package com.gu.contentapi.circe
 
 import scala.language.experimental.macros
-import scala.reflect.macros.whitebox.Context
+import scala.reflect.macros.{blackbox,whitebox}
 import shapeless.LabelledGeneric
-import com.twitter.scrooge.ThriftStruct
+import com.twitter.scrooge.{ThriftEnum, ThriftStruct}
+import io.circe.Decoder
 
 /**
- * Macro to help Circe find LabelledGeneric instances for Scrooge Thrift structs.
- * The macro is needed because Scrooge generates non-sealed traits.
- *
- * Scrooge represents
- *
- * {{{
- * struct Foo { 
- *   1: string a
- *   2: i32 b
- * }
- * }}}
- *
- * As a trait plus a companion object with a nested `Immutable` class.
- * Roughly:
- *
- * {{{
- * object Foo {
- *   def apply(a: String, b: Int) = new Immutable(a, b)
- *   class Immutable(val a: String, val b: Int) extends Foo
- * }
- *
- * trait Foo {
- *   def a: String
- *   def b: Int
- * }
- * }}}
- *
- * The code to deal with this situation was copy-pasted from
- * https://github.com/travisbrown/scrooge-circe-demo
- */
+  * Macros for Circe deserialization of various Scrooge-generated classes.
+  */
 object CirceScroogeHelper {
 
+  /**
+    * Macro to help Circe find LabelledGeneric instances for Scrooge Thrift structs.
+    * The macro is needed because Scrooge generates non-sealed traits.
+    *
+    * Scrooge represents
+    *
+    * {{{
+    * struct Foo {
+    *   1: string a
+    *   2: i32 b
+    * }
+    * }}}
+    *
+    * As a trait plus a companion object with a nested `Immutable` class.
+    * Roughly:
+    *
+    * {{{
+    * object Foo {
+    *   def apply(a: String, b: Int) = new Immutable(a, b)
+    *   class Immutable(val a: String, val b: Int) extends Foo
+    * }
+    *
+    * trait Foo {
+    *   def a: String
+    *   def b: Int
+    * }
+    * }}}
+    *
+    * The code to deal with this situation was copy-pasted from
+    * https://github.com/travisbrown/scrooge-circe-demo
+    */
   implicit def materializeStructGen[A <: ThriftStruct]: LabelledGeneric[A] = macro materializeStructGen_impl[A]
 
-  def materializeStructGen_impl[A: c.WeakTypeTag](c: Context): c.Tree = {
+  def materializeStructGen_impl[A: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
     import c.universe._
 
     val A = weakTypeOf[A]
@@ -58,6 +62,32 @@ object CirceScroogeHelper {
         def from(r: Repr): $A = np.from(r)
       }
     }"""
+  }
+
+  /**
+    * Macro to provide custom decoding of Thrift enums
+    */
+  implicit def decodeThriftEnum[A <: ThriftEnum]: Decoder[A] = macro decodeThriftEnum_impl[A]
+
+  def decodeThriftEnum_impl[A: c.WeakTypeTag](c: blackbox.Context): c.Tree = {
+    import c.universe._
+
+    val A = weakTypeOf[A]
+    val typeName = A.typeSymbol.name.toString
+    val valueOf = A.companion.member(TermName("valueOf"))
+    val unknown = A.companion.member(TermName(s"EnumUnknown$typeName"))
+
+    q"""
+    _root_.io.circe.Decoder.instance((cursor: _root_.io.circe.HCursor) => {
+      cursor.focus.asString match {
+        case _root_.scala.Some(value) =>
+          val withoutHyphens = value.replaceAllLiterally("-", "")
+          _root_.cats.data.Xor.right($valueOf(withoutHyphens).getOrElse($unknown.apply(-1)))
+        case _ =>
+          _root_.cats.data.Xor.left(_root_.io.circe.DecodingFailure($typeName, cursor.history))
+      }
+    })
+    """
   }
 
 }
