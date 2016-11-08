66e987029d2a73d8e17e725f4f6ecf69d7d707cf
Luke Taylor <luke.taylor@guardian.co.uk>
2016-11-08 18:12:08 +0000

    Content api models - add auxiliary atom event model.

---
+++ models/src/main/thrift/events/flexible/auxiliaryAtomEvent.thrift
@@ -0,0 +1,17 @@
+namespace scala com.gu.auxiliaryatom.model.auxiliaryatomevent.v1
+
+enum EventType {
+    ADD = 0,
+    REMOVE = 1
+}
+
+struct AuxiliaryAtom {
+    1: required string atomId
+    2: required string atomType
+}
+
+struct AuxiliaryAtomEvent {
+    1: required string contentId
+    2: required EventType eventType
+    3: required list<AuxiliaryAtom> auxiliaryAtom
+}
