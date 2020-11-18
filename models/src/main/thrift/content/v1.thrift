1d199c2a5dd05ee140bd122176029f0ea7f27075
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2020-11-18 12:02:18 +0000

    Document why the tracking field is optional and when it is likely to appear.

--- models/src/main/thrift/content/v1.thrift
+++ models/src/main/thrift/content/v1.thrift
@@ -759,6 +759,11 @@ struct BlockElement {
 
     20: optional ContentAtomElementFields contentAtomTypeData
 
+    /*
+     * Optional field which may appear on a block element with a html field containing third party embed code.
+     * Likely to appear on embed and other rich media type elements.
+     * Does not appear on text block elements as the html field of a text element does not contain embed code.
+     */
     21: optional EmbedTracking tracking
 
 }
