626994ca024922f52a2b90325049650dea78a487
frederickobrien <frederick.obrien@guardian.co.uk>
2024-02-22 12:51:28 +0000

    Recipe schema thrift fields
    Companion to https://github.com/guardian/content-api/pull/2858

--- models/src/main/thrift/content/schemaorg.thrift
+++ models/src/main/thrift/content/schemaorg.thrift
@@ -8,4 +8,29 @@ struct SchemaRecipe {
     3: optional string name
     4: optional string description
     5: optional string image
+    6: optional string datePublished
+    7: optional string url
+    8: optional list<string> recipeCategory
+    9: optional list<string> recipeCuisine
+    10: optional list<string> recipeIngredient
+    11: optional list<RecipeStep> recipeInstructions
+    12: optional list<string> recipeYield
+    13: optional string prepTime
+    14: optional string cookTime
+    15: optional string totalTime
+    16: optional AuthorInfo author
+}
+
+struct RecipeStep {
+    1: required string _at_type
+    2: required string text
+    3: optional string name
+    4: optional string url
+    5: optional list<string> image
+}
+
+struct AuthorInfo {
+    1: required string _at_type
+    2: required string name
+    3: optional list<string> sameAs
 }
